6dc71f4149c9ea9ad1934ff49706874011746448
swain <iamswain@hotmail.com>
2019-07-01 20:03:48 -0700

    Updates

---
+++ precache-manifest.d5b2939410b3ba08a0edd251b5b01a76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99b380695d62ee2d88a6d1ab36600e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24a9f7b0e85513355cb",
+    "url": "/static/js/2.643d4940.chunk.js"
+  },
+  {
+    "revision": "bd8c7350053f104af85a",
+    "url": "/static/js/main.690d4b2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2abd0a0d16169240f4e3667c4f5807e3",
+    "url": "/static/media/lacan.2abd0a0d.png"
+  }
+]);
