5257d4ff2449f1e434a0754e11f8260fd6e1fc23
swain <iamswain@hotmail.com>
2021-01-31 21:04:12 +0900

    2020 update

--- src/Face.js
+++ src/Face.js
@@ -5,48 +5,50 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLinkedin,
   faGithub,
-  faFacebook
+  faFacebook,
 } from "@fortawesome/free-brands-svg-icons";
 
-export default props => (
-  <Container>
-    <Row style={{ alignItems: "center" }}>
-      <Col>
-        <img
-          src={Lacan}
-          alt="img"
-          style={{
-            objectFit: "contain",
-            width: "inherit",
-            borderRadius: 20,
-            transform: "scale(0.8)"
-          }}
-        />
-      </Col>
-      <Col>
-        <h1>Swain Hwang</h1>
-        <h2>Web Dev.</h2>
-        <h3>Javascript</h3>
-        <Row>
-          <Col>
-            {[
-              { site: "https://linkedin.com/in/iamswain", icon: faLinkedin },
-              { site: "https://github.com/iamswain25", icon: faGithub },
-              { site: "https://facebook.com/iamswain", icon: faFacebook }
-            ].map((i, index) => (
-              <a
-                href={i.site}
-                target="_blank"
-                rel="noopener noreferrer"
-                key={index}
-                style={{ marginRight: 10 }}
-              >
-                <FontAwesomeIcon icon={i.icon} size="3x" />
-              </a>
-            ))}
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Container>
-);
+export default function Face(props) {
+  return (
+    <Container>
+      <Row style={{ alignItems: "center" }}>
+        <Col>
+          <img
+            src={Lacan}
+            alt="img"
+            style={{
+              objectFit: "contain",
+              width: "inherit",
+              borderRadius: 20,
+              transform: "scale(0.8)",
+            }}
+          />
+        </Col>
+        <Col>
+          <h1>Swain Hwang</h1>
+          <h2>Web Dev.</h2>
+          <h3>Javascript</h3>
+          <Row>
+            <Col>
+              {[
+                { site: "https://linkedin.com/in/iamswain", icon: faLinkedin },
+                { site: "https://github.com/iamswain25", icon: faGithub },
+                { site: "https://facebook.com/iamswain", icon: faFacebook },
+              ].map((i, index) => (
+                <a
+                  href={i.site}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  key={index}
+                  style={{ marginRight: 10 }}
+                >
+                  <FontAwesomeIcon icon={i.icon} size="3x" />
+                </a>
+              ))}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
--- src/Year.js
+++ src/Year.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-export default ({ year, lists }) => (
-  <section>
-    <h1>{year}</h1>
-    <ul>
-      {lists.map(({ title, website, desc }, i) => (
-        <li key={i}>
-          <h2>{title}</h2>
-          <h4>
-            <a href={website}>{website}</a>
-          </h4>
-          <h5>{desc}</h5>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+export default function Year({ year, lists }) {
+  return (
+    <section>
+      <h1>{year}</h1>
+      <ul>
+        {lists.map(({ title, website, desc }, i) => (
+          <li key={i}>
+            <h2>{title}</h2>
+            <h4>
+              <a href={website} target="_blank" rel="noopener noreferrer">
+                {website}
+              </a>
+            </h4>
+            <h5>{desc}</h5>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
